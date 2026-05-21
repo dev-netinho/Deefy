28c9dc2c231dac90a6af28aba00e7b100f54727e
saylonrb <saylonbatista07@gmail.com>
2026-05-20 21:22:27 -0300

    feat(Backend): implementação de validação de email, configuração de API Resend e remoção do Spring mail

--- backend/src/main/java/br/com/deefy/config/SecurityConfig.java
+++ backend/src/main/java/br/com/deefy/config/SecurityConfig.java
@@ -55,8 +55,6 @@ public class SecurityConfig {
 
                         .requestMatchers("/api/v1/auth/**").permitAll()
 
-                        .requestMatchers("/error").permitAll()
-
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 
                         .anyRequest().authenticated()
