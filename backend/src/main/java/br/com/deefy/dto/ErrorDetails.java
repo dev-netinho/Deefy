c15a883afb49b2fce3d1e615840337ba8967f98b
mauricio <mauricioaraujolucas@gmail.com>
2026-05-24 11:00:54 -0300

    feat: padroniza tratamento global de exceções da API

--- backend/src/main/java/br/com/deefy/dto/ErrorDetails.java
+++ backend/src/main/java/br/com/deefy/dto/ErrorDetails.java
@@ -1,9 +1,23 @@
 package br.com.deefy.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record ErrorDetails(
+
+        // Hora do erro
         LocalDateTime timestamp,
-        String message,
-        String details
-) {}
+
+        // Status do erro
+        int status,
+
+        // Tipo do Erro
+        String error,
+
+        // Mensagens de erro
+        List<String> messages,
+
+        // Caminho do erro
+        String path
+) {
+}
